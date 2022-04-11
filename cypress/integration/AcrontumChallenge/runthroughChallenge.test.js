5d5c348bdaf2fa2365e18061ae274984ec2b9de6
Ismael Balikci <india7bravo@gmail.com>
2022-04-11 22:26:30 +0200

    Test complete

---
+++ cypress/integration/AcrontumChallenge/runthroughChallenge.test.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+describe("User journey QA challenge", () => {
+    beforeEach(() => {
+        cy.goTo()
+    })
+
+    it('The user searches for one of the available books, opens it, adds to collection and verifies', () => {
+        // User goes to the book store, searches for
+        // the book and clicks on it
+        cy.contains('Go To Book Store').scrollIntoView().click({force: true})
+        cy.get('#searchBox').type('Elo')
+        cy.contains("Eloquent").click({force: true})
+
+        // Expands the elements side-bar menu to extend
+        // the page and help get to the profile section
+        cy.get(':nth-child(1) > .group-header > .header-wrapper').click()
+
+        // Adds book to my collection
+        cy.get('.text-right > #addNewRecordButton').click({force: true})
+
+        // Ensures the alert says "Book added to your collection."
+        cy.on('window:alert',(txt)=>{
+            expect(txt).to.contains('Book added to your collection.');
+         })
+
+        // Extends the menu to get to the profile section
+        cy.contains('Book Store Application').click({force: true})
+        cy.get('li').eq(31).click()
+
+        // Makes sure the book is in my profile
+        cy.contains('.main-header', 'Profile').should('be.visible')
+        cy.contains("Eloquent JavaScript, Second Edition").should('be.visible')
+    })
+
+    it('The user gets logged in and taken to the profile page to verify the book', () => {
+        // Makes sure the book is in my profile
+        cy.contains('.main-header', 'Profile').should('be.visible')
+        cy.contains("Eloquent JavaScript, Second Edition").should('be.visible')
+
+        // Deletes the book for the next runthrough and
+        // dismisses the modal
+        cy.get('#delete-record-undefined > svg').click()
+        cy.get('#closeSmallModal-ok').click()
+
+        // Ensures the alert says "Book deleted."
+        cy.on('window:alert',(txt)=>{
+            expect(txt).to.contains('Book deleted.');
+         })
+    })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,33 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('goTo', (label) => {
+    cy.visit('https://demoqa.com/login').wait(1000)
+    cy.contains('Login').should('be.visible')
+    cy.get('#userName').click().type('India7Bravo')
+    cy.get('#password').click().type('9CL5RDy4SgtXw2E!@')
+    cy.get('#login').click()
+  });
